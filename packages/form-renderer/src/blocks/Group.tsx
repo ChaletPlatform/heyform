de2ca1cc48def23d0e7fbc712245d57ee423a64d
ihaseeb88 <eng@getchalet.com>
2026-05-26 01:52:09 +0500

    refactor: simplify Branding component to always return null
    Removes conditional rendering in the Branding component, ensuring it consistently returns null. This change streamlines the component's logic and prepares for potential future updates.

--- packages/form-renderer/src/blocks/Group.tsx
+++ packages/form-renderer/src/blocks/Group.tsx
@@ -150,7 +150,7 @@ const GroupChildField: FC<{ field: any; t: (s: string) => string }> = ({ field, 
 
     case FieldKindEnum.STATEMENT:
       return (
-        <div className="heyform-group-child heyform-group-child-statement">
+        <div className="heyform-group-child heyform-group-child-statement text-xs">
           {label}
         </div>
       )
--- packages/form-renderer/src/views/Branding.tsx
+++ packages/form-renderer/src/views/Branding.tsx
@@ -10,22 +10,7 @@ export const Branding: FC = () => {
   const { state } = useStore()
   const { t } = useTranslation()
 
-  if (state.settings?.removeBranding) {
-    return null
-  }
-
-  return (
-    <a className="heyform-branding" href="https://heyform.net/?ref=badge" target="_blank">
-      <Trans
-        t={t}
-        i18nKey="Made with HeyForm"
-        components={{
-          icon: <LogoIcon className="inline h-4 w-4" />,
-          span: <span className="font-medium" />
-        }}
-      />
-    </a>
-  )
+  return null
 }
 
 export const WelcomeBranding: FC = () => {
